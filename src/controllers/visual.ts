9cf404a5d0e4b3474f7df146abf3ff53fc647291
mwerensteijn <mitchellwerensteijn@msn.com>
2017-12-07 17:29:00 +0100

    Gets Vision result from Watson

--- src/controllers/visual.ts
+++ src/controllers/visual.ts
@@ -4,19 +4,27 @@ import * as watson from 'watson-developer-cloud';
 const router: express.Router = express.Router();
 
 var visual_recognition = watson.visual_recognition({
-    api_key: '{api_key}',
+    api_key: 'ff0e720cf4b833cb6f59c16587a36c847acb1403',
     version: 'v3',
     version_date: '2016-05-20'
 });
 
 router.post('/test', (req, res) => {
-    console.log(req.files);
-    res.json({});
+    var params = {
+        images_file: req.files[0].buffer
+    };
 
-    // visual_recognition.classify(params, function(err: any, res: any) {
-    //     if (err) console.log(err);
-    //     else console.log(JSON.stringify(res, null, 2));
-    // });
+    var result = {};
+    visual_recognition.classify(params, function(err: any, res: any) {
+        if (err) {
+            console.log(err);
+        } else {
+            result = JSON.stringify(res, null, 2);
+            console.log(result);
+        }
+    });
+
+    res.json(result);
 });
 
 export const routerVisual = router;
