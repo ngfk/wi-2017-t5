62c033bad7ec8059d26b5cd7c794a362fc661784
Rick Koster <ngf.koster@outlook.com>
2017-12-07 15:47:41 +0100

    disable jwt authorization for /api/visual/test

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -6,5 +6,5 @@ if (!JWT_SECRET) throw new Error('No JWT_SECRET env variable defined.');
 const secret = JWT_SECRET;
 
 export const middlewareJwt: RequestHandler = jwt({ secret }).unless({
-    path: ['/api/login']
+    path: ['/api/login', '/api/visual/test']
 });
