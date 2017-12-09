cb1db0763bde0518dedfa9ab574a7080f3b8c0d5
mwerensteijn <mitchellwerensteijn@msn.com>
2017-12-09 14:53:19 +0100

    City Profile class added

--- src/models/city-profile.ts
+++ src/models/city-profile.ts
@@ -1,4 +1,4 @@
-export interface CityProfile {
+export interface CityProfilePayload {
     name: string;
     museums: Museum[];
     monuments: Monument[];
@@ -31,3 +31,25 @@ export interface Misc {
     price: number;
     rating: number;
 }
+
+export class CityProfile {
+    public readonly name: string;
+    public readonly museums: Museum[];
+    public readonly monuments: Monument[];
+    public readonly clubs: Club[];
+    public readonly miscs: Misc[];
+    public readonly priceRange: number;
+    public readonly ratings: number[];
+
+    constructor(payload: CityProfilePayload) {
+        this.name = payload.name;
+        this.museums = payload.museums;
+        this.monuments = payload.monuments;
+        this.clubs = payload.clubs;
+        this.miscs = payload.miscs;
+        this.priceRange = payload.priceRange;
+        this.ratings = payload.ratings;
+    }
+}
+
+export const cities = new Map<string, CityProfile>();
