b1a63fcaf685659e12d80fb65ba638bf69ee2d87
Rick Koster <ngf.koster@outlook.com>
2017-12-10 15:15:37 +0100

    start matcher

--- src/components/matcher.ts
+++ src/components/matcher.ts
@@ -1,8 +1,20 @@
-import { CityProfile } from '../models/city-profile';
-import { UserProfile } from '../models/user-profile';
+import { cities, CityProfile } from '../models/city-profile';
+import { profiles, UserProfile } from '../models/user-profile';
+import { UserToken } from '../models/user-token';
 
 export class Matcher {
-    public match(user: UserProfile, cities: CityProfile[]): CityProfile {
+    private user: UserProfile;
+    private cities: CityProfile[];
+
+    constructor(user: UserToken) {
+        const profile = profiles.get(user.id);
+        if (!profile) throw new Error('Unknown user: ' + user.id);
+
+        this.user = profile;
+        this.cities = Array.from(cities.entries());
+    }
+
+    public match(): CityProfile {
         throw new Error('unimplemented method');
     }
 }
