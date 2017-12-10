1d16b68bb20716317bcc67c1296e601f91c6b5cd
Rick Koster <ngf.koster@outlook.com>
2017-12-10 17:10:13 +0100

    rename

--- src/models/data-store.ts
+++ src/models/data-store.ts
@@ -1,6 +1,6 @@
-import * as denHaag from '../Cities/Den haag.json';
-import * as leiden from '../Cities/Leiden.json';
-import * as utrecht from '../Cities/Utrecht.json';
+import * as denHaag from '../cities/Den haag.json';
+import * as leiden from '../cities/Leiden.json';
+import * as utrecht from '../cities/Utrecht.json';
 import { Conversation } from '../components/conversation';
 import { City, CityProfile } from './city-profile';
 import { UserProfile } from './user-profile';
