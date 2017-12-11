190fa30b6378f362bbd8ebeb429b921d4753a9ce
Rick Koster <ngf.koster@outlook.com>
2017-12-11 14:42:09 +0100

    delete matcher model

--- src/models/matcher.ts
+++
@@ -1,7 +0,0 @@
-import { CityProfile } from './city-profile';
-import { UserProfile } from './user-profile';
-
-export interface ProfileMatcher {
-    userProfile: UserProfile;
-    cityProfiles: CityProfile[];
-}
